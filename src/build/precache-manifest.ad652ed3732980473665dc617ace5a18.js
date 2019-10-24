bf8e1b17423ac73234d7ad6bdd51d941554f06b5
Vishnu S <vish29namivun@gmail.com>
2019-10-25 00:38:19 +0530

    second

---
+++ src/build/precache-manifest.ad652ed3732980473665dc617ace5a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cc2362d34f6954a50c3fe57716a4a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50bbe24ce368d23adc56",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "bb03e6fa3f0cf829c117",
+    "url": "/static/js/2.8f40e0e1.chunk.js"
+  },
+  {
+    "revision": "50bbe24ce368d23adc56",
+    "url": "/static/js/main.db7c3d48.chunk.js"
+  },
+  {
+    "revision": "19dffa7a1e78686fab4a",
+    "url": "/static/js/runtime-main.ca086102.js"
+  }
+]);
